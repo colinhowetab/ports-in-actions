9636749e43817a87fffd66d1a12786c060fe79c5
Colin Howe <88904+colinhowe@users.noreply.github.com>
2019-10-02 20:29:24 +0100

    use correct hostname for chromedriver

--- selenium-test.py
+++ selenium-test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 driver = driver = webdriver.Remote(
-   command_executor='http://127.0.0.1:4444/wd/hub',
+   command_executor='http://chromedriver:4444/wd/hub',
    desired_capabilities=DesiredCapabilities.CHROME)
 driver.get("172.18.0.2:5000")
 print 'I got source!'
