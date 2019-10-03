0d5a10a20bcd7a325a60c1da2c189398a3ea7916
Colin Howe <88904+colinhowe@users.noreply.github.com>
2019-10-03 10:51:16 +0100

    echo hostname and also try container as the hostname

--- selenium-test.py
+++ selenium-test.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 driver = driver = webdriver.Remote(
    command_executor='http://chromedriver:4444/wd/hub',
    desired_capabilities=DesiredCapabilities.CHROME)
-driver.get("http://172.18.0.2:5000")
+driver.get("http://container:5000")
 print 'I got source!'
 print driver.page_source
 driver.close()
